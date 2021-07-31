8b333233d7282a943f9f3017258fd18b5e305db4
unknown <jeehea99@naver.com>
2021-08-01 00:05:15 +0900

    지혜 브랜치 생성

--- app/src/main/java/com/example/duksunggoodsplatform_2021_android/MainActivity.kt
+++ app/src/main/java/com/example/duksunggoodsplatform_2021_android/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.duksunggoodsplatform_2021_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-// 첫 화면
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
