97dd81f30c0a4fe0d97d59ef3934552c0baf0b68
serim53 <tpfla9953@naver.com>
2021-08-03 15:20:46 +0900

    카테고리별 굿즈 리스트

--- app/src/main/java/com/example/duksunggoodsplatform_2021_android/MainActivity.kt
+++ app/src/main/java/com/example/duksunggoodsplatform_2021_android/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.duksunggoodsplatform_2021_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.duksunggoodsplatform_2021_android.category.CategoryFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment, CategoryFragment())
+            .commit()
     }
 }
---
+++ app/src/main/java/com/example/duksunggoodsplatform_2021_android/category/Data.kt
@@ -0,0 +1,5 @@
+package com.example.duksunggoodsplatform_2021_android.category
+
+import android.graphics.drawable.Drawable
+
+class Data(val name: String, val price: String, val photo: Int)
