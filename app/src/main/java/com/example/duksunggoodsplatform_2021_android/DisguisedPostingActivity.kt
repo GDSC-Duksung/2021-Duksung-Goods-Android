91bd41ef77b74e7d55a90139aa6e833a1c651a34
hjh1161514 <hjh1161514@naver.com>
2021-08-08 00:32:14 +0900

    가수요조사 & 실수요조사 포스팅 UI

---
+++ app/src/main/java/com/example/duksunggoodsplatform_2021_android/DisguisedPostingActivity.kt
@@ -0,0 +1,10 @@
+package com.example.duksunggoodsplatform_2021_android
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DisguisedPostingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_disguised_demand_posting)
+    }
+}
