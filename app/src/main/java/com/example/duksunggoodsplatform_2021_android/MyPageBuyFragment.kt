c54a205189df0aad84b434188c85795cc0a445f0
hjh1161514 <hjh1161514@naver.com>
2021-08-07 22:13:15 +0900

    마이페이지 구매 & 판매 UI

---
+++ app/src/main/java/com/example/duksunggoodsplatform_2021_android/MyPageBuyFragment.kt
@@ -0,0 +1,17 @@
+package com.example.duksunggoodsplatform_2021_android
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class MyPageBuyFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_mypage_buy, container, false)
+    }
+}
---
+++ app/src/main/java/com/example/duksunggoodsplatform_2021_android/MyPageFragment.kt
@@ -0,0 +1,24 @@
+package com.example.duksunggoodsplatform_2021_android
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager.widget.PagerAdapter
+import kotlinx.android.synthetic.main.fragment_mypage.*
+
+
+class MyPageFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_mypage, container, false)
+    }
+
+}
