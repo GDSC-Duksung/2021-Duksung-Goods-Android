bacd17e1305c161f31320780732897ec2a4d4cc0
rosesua318 <rosesua@duksung.ac.kr>
2021-08-09 17:21:42 +0900

    홍보뷰 등록 UI

---
+++ app/src/main/java/com/example/duksunggoodsplatform_2021_android/home/AddPromotionActivity.kt
@@ -0,0 +1,12 @@
+package com.example.duksunggoodsplatform_2021_android.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.duksunggoodsplatform_2021_android.R
+
+class AddPromotionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_promotion)
+    }
+}
