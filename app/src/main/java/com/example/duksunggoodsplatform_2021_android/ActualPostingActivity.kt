69f7bae22d33058eaf40f5883fda00c65505a13e
hjh1161514 <hjh1161514@naver.com>
2021-08-18 20:27:32 +0900

    실수요조사 포스팅 & 가수요조사 포스팅 기본 기능

--- app/src/main/java/com/example/duksunggoodsplatform_2021_android/ActualPostingActivity.kt
+++ app/src/main/java/com/example/duksunggoodsplatform_2021_android/ActualPostingActivity.kt
@@ -1,21 +1,27 @@
 package com.example.duksunggoodsplatform_2021_android
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
+import com.example.duksunggoodsplatform_2021_android.dialog.CustomDialog
+import kotlinx.android.synthetic.main.activity_actual_demand_posting.*
 
 class ActualPostingActivity : AppCompatActivity() {
+    val spinner: Spinner = findViewById(R.id.spinner_actual_posting_category)
+    val items = resources.getStringArray(R.array.category_array)
+    val myAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, items)
+    var SET_IMAGE = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_actual_demand_posting)
 
         // Spinner
-        val spinner: Spinner = findViewById(R.id.spinner_actual_posting_category)
-        val items = resources.getStringArray(R.array.category_array)
-        val myAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, items)
         myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.adapter = myAdapter
         spinner.setSelection(3)
@@ -41,6 +47,36 @@ class ActualPostingActivity : AppCompatActivity() {
 
             override fun onNothingSelected(parent: AdapterView<*>) {
                 Log.d("SpinnerTag", "onNothingSelected")
+            }
+        }
+
+        // 뒤로가기 버튼, 취소하기 버튼
+        img_back_actual_demand_posting.setOnClickListener{
+            finish()
+        }
+
+        btn_cancel_actual_posting.setOnClickListener {
+            finish()
+        }
+
+        // 등록하기 버튼
+        btn_register_actual_posting.setOnClickListener {
+            // 성공
+            if(et_actual_posting_title.text.isNotEmpty() && et_actual_posting_content.text.isNotEmpty() && SET_IMAGE == true && et_actual_posting_date.text.isNotEmpty()
+                && et_actual_posting_price.text.isNotEmpty() && cb_actual_posting.isChecked) {
+                val dialog = CustomDialog("실수요조사 포스팅에 \n 성공하였습니다.")
+                // 버튼 클릭 이벤트 설정
+                dialog.setButtonClickListener(object : CustomDialog.OnButtonClickListener {
+                    override fun onButtonClicked() {
+                        // 서버에 데이터 전송
+                        finish()
+                    }
+                })
+                dialog.show(supportFragmentManager, "CustomDialog")
+            } else{
+                // 실패
+                val dialog = CustomDialog("실수요조사 포스팅에 \n 실패하였습니다. \n 모든 내용을 입력해주세요. ")
+                dialog.show(supportFragmentManager, "CustomDialog")
             }
         }
     }
