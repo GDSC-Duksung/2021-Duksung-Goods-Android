a0f6c399f87ec143666b403d4fabf9f40b13ed06
hjh1161514 <hjh1161514@naver.com>
2021-08-28 00:04:32 +0900

    가수요조사 포스팅 기능추가 - 이미지 첨부

--- app/src/main/java/com/example/duksunggoodsplatform_2021_android/DisguisedPostingActivity.kt
+++ app/src/main/java/com/example/duksunggoodsplatform_2021_android/DisguisedPostingActivity.kt
@@ -1,15 +1,26 @@
 package com.example.duksunggoodsplatform_2021_android
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.example.duksunggoodsplatform_2021_android.dialog.CustomDialog
 import kotlinx.android.synthetic.main.activity_disguised_demand_posting.*
+import java.io.IOException
 
 class DisguisedPostingActivity : AppCompatActivity() {
+
+    private val PICK_IMAGE1 = 1
+    private val PICK_IMAGE2 = 2
+    private val PICK_IMAGE3 = 3
+    private val PICK_IMAGE4 = 4
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_disguised_demand_posting)
@@ -57,6 +68,31 @@ class DisguisedPostingActivity : AppCompatActivity() {
             finish()
         }
 
+        // 사진 추가하기
+        btn_disguised_posting_img1.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.setType(MediaStore.Images.Media.CONTENT_TYPE)
+            startActivityForResult(intent, PICK_IMAGE1)
+        }
+
+        btn_disguised_posting_img2.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.setType(MediaStore.Images.Media.CONTENT_TYPE)
+            startActivityForResult(intent, PICK_IMAGE2)
+        }
+
+        btn_disguised_posting_img3.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.setType(MediaStore.Images.Media.CONTENT_TYPE)
+            startActivityForResult(intent, PICK_IMAGE3)
+        }
+
+        btn_disguised_posting_img4.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.setType(MediaStore.Images.Media.CONTENT_TYPE)
+            startActivityForResult(intent, PICK_IMAGE4)
+        }
+
         // 등록하기 버튼
         btn_register_disguised_posting.setOnClickListener {
             // 성공
@@ -82,6 +118,60 @@ class DisguisedPostingActivity : AppCompatActivity() {
                     }
                 })
                 dialog.show(supportFragmentManager, "CustomDialog")
+            }
+        }
+    }
+
+    var selectedPicUri1 : Uri? = null
+    var selectedPicUri2 : Uri? = null
+    var selectedPicUri3 : Uri? = null
+    var selectedPicUri4 : Uri? = null
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode==PICK_IMAGE1){
+            data?.let{
+                selectedPicUri1 = it.data!!
+                Glide.with(this).load(selectedPicUri1).into(btn_disguised_posting_img1)
+            }
+            try{
+                //uploadImage()
+            }catch (e : IOException){
+                e.printStackTrace()
+            }
+        }
+        if(requestCode==PICK_IMAGE2){
+            data?.let{
+                selectedPicUri2 = it.data!!
+                Glide.with(this).load(selectedPicUri2).into(btn_disguised_posting_img2)
+            }
+            try{
+                //uploadImage()
+            }catch (e : IOException){
+                e.printStackTrace()
+            }
+        }
+        if(requestCode==PICK_IMAGE3){
+            data?.let{
+                selectedPicUri3 = it.data!!
+                Glide.with(this).load(selectedPicUri3).into(btn_disguised_posting_img3)
+            }
+            try{
+                //uploadImage()
+            }catch (e : IOException){
+                e.printStackTrace()
+            }
+        }
+        if(requestCode==PICK_IMAGE4){
+            data?.let{
+                selectedPicUri4 = it.data!!
+                Glide.with(this).load(selectedPicUri4).into(btn_disguised_posting_img4)
+            }
+            try{
+                //uploadImage()
+            }catch (e : IOException){
+                e.printStackTrace()
             }
         }
     }
