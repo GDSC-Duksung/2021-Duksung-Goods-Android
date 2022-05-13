1f013be079b76b866203d381a01e443f58d89221
jeeheaG <jeehea99@naver.com>
2022-05-13 22:53:01 +0900

    feat: GoodsDetailFragment Binding many images Done

--- app/src/main/java/com/example/duksunggoodsplatform_2021_android/goodsEx/GoodsDetailFragment.kt
+++ app/src/main/java/com/example/duksunggoodsplatform_2021_android/goodsEx/GoodsDetailFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.setFragmentResultListener
 import com.bumptech.glide.Glide
@@ -21,14 +22,21 @@ class GoodsDetailFragment : Fragment() {
         super.onCreate(savedInstanceState)
         setFragmentResultListener("goodsInfo") { requestKey, bundle ->
             title = bundle.getString("title") ?: "no data"
-            imgList = bundle.getStringArrayList("imgList") ?: arrayListOf("no data")//TODO : 두번째 값부터 사용할 것 - 리스트 아니고 하난데?
+            imgList = bundle.getStringArrayList("imgList") ?: arrayListOf("no data")
             description = bundle.getString("description") ?: "no data"
 
             Log.d("로그detail---", "title: ${title}")
 
             binding.tvGoodsDetailTitle.text = title
             binding.tvGoodsDetailDescription.text = description
-            activity?.let { Glide.with(it).load(imgList?.get(0)).into(binding.ivGoodsDetail) }
+
+            val layout = binding.llGoodsDetailFragment
+            for(url in imgList){
+                var iv = ImageView(activity)
+                activity?.let { Glide.with(it).load(url).into(iv) }
+                layout.addView(iv)
+                //iv.setImageResource()
+            }
 
             // Do something with the result
         }
@@ -37,6 +45,13 @@ class GoodsDetailFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentGoodsDetailBinding.inflate(inflater, container, false)
 
+/*        val layout = binding.llGoodsDetailFragment
+        for(url in imgList){
+            var iv = ImageView(activity)
+            activity?.let { Glide.with(it).load(url).into(iv) }
+            layout.addView(iv)
+            //iv.setImageResource()
+        }*/
 
         return binding.root
     }
